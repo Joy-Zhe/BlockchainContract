2331c7c91deae9e69d26059c8847eff8283293ff
Dzjgz <94751863+Dzjgz@users.noreply.github.com>
2023-09-18 22:08:43 +0800

    Update sdk.go

--- application/server/blockchain/sdk.go
+++ application/server/blockchain/sdk.go
@@ -69,3 +69,45 @@ func ChannelQuery(fcn string, args [][]byte) (channel.Response, error) {
 	//返回链码执行后的结果
 	return resp, nil
 }
+//新建函数，完成区块链通道选择
+// ChannelExecute 区块链交互
+func ChannelExecute_new(fcn string, self_channelName string, args [][]byte) (channel.Response, error) {
+	// 创建客户端，表明在通道的身份
+	ctx := sdk.ChannelContext(self_channelName, fabsdk.WithUser(user))
+	cli, err := channel.New(ctx)
+	if err != nil {
+		return channel.Response{}, err
+	}
+	// 对区块链账本的写操作（调用了链码的invoke）
+	resp, err := cli.Execute(channel.Request{
+		ChaincodeID: chainCodeName,
+		Fcn:         fcn,
+		Args:        args,
+	}, channel.WithTargetEndpoints(endpoints...))
+	if err != nil {
+		return channel.Response{}, err
+	}
+	//返回链码执行后的结果
+	return resp, nil
+}
+
+// ChannelQuery 区块链查询
+func ChannelQuery_new(fcn string, self_channelName string, args [][]byte) (channel.Response, error) {
+	// 创建客户端，表明在通道的身份
+	ctx := sdk.ChannelContext(self_channelName, fabsdk.WithUser(user))
+	cli, err := channel.New(ctx)
+	if err != nil {
+		return channel.Response{}, err
+	}
+	// 对区块链账本查询的操作（调用了链码的invoke），只返回结果
+	resp, err := cli.Query(channel.Request{
+		ChaincodeID: chainCodeName,
+		Fcn:         fcn,
+		Args:        args,
+	}, channel.WithTargetEndpoints(endpoints...))
+	if err != nil {
+		return channel.Response{}, err
+	}
+	//返回链码执行后的结果
+	return resp, nil
+}
