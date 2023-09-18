ea9772594d951eeec6d26d47d999d15113a5d573
Dzjgz <94751863+Dzjgz@users.noreply.github.com>
2023-09-18 21:43:58 +0800

    Update chaincode_test.go

--- chaincode_2/chaincode_test.go
+++ chaincode_2/chaincode_test.go
@@ -35,7 +35,6 @@ func checkInvoke(t *testing.T, stub *shim.MockStub, args [][]byte) pb.Response {
 	return res
 }
 
-// 测试链码初始化
 func TestBlockChainRealEstate_Init(t *testing.T) {
 	stub := initTest(t)
 	fmt.Println(fmt.Sprintf("1、初始化\n%s",
@@ -48,11 +47,12 @@ func TestBlockChainRealEstate_Init(t *testing.T) {
 			[]byte("ContractSanction_upload"),
 			[]byte("{\"contractname\":\"NEW1\"," +
 				"\"contractcontent\":\"6b34\",\"creatercompanyname\":" +
-				"\"taobao\",\"creatercompanysign\":\"mayun\",\"creatertime\":" +
+				"\"taobao\",\"creatercompanysign\":\"mayun\",\"signtime\":" +
 				"\"2023-09-15 20:51:15\"}"),
 		}).Payload)))
 	fmt.Println(fmt.Sprintf("3、查询合同\n%s",
 		string(checkInvoke(t, stub, [][]byte{
 			[]byte("QueryContract_amongcompany"),
+			[]byte("NEW1"),
 		}).Payload)))
 }
