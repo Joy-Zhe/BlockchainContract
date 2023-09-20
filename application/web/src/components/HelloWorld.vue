0c18780251c320b94c5d6b2327c15bfc48f45236
Joy Zheng <z13676788757@163.com>
2023-09-20 15:00:31 +0800

    rewrite loginPage

---
+++ application/web/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <el-container class="test">
+    <el-header>欢迎使用区块链企业合同签订系统</el-header>
+    <el-main>
+      <el-button class="Login">
+        登录
+      </el-button>
+    </el-main>
+  </el-container>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .el-header{
+    background-color: #2c3e50;
+    color: #B3C0D1;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-main{
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
