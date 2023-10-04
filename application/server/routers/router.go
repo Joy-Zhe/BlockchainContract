b32546d7ceda9b7cd990b87949e6b9e1daf7bd18
Dzjgz <94751863+Dzjgz@users.noreply.github.com>
2023-10-04 22:39:45 +0800

    Update router.go

--- application/server/routers/router.go
+++ application/server/routers/router.go
@@ -24,11 +24,11 @@ func InitRouter() *gin.Engine {
 		apiV1.POST("/queryDonatingList", v1.QueryDonatingList)
 		apiV1.POST("/queryDonatingListByGrantee", v1.QueryDonatingListByGrantee)
 		apiV1.POST("/updateDonating", v1.UpdateDonating)
-		apiV1.POST("/StartContract", v1.StartContract)
-		apiV1.POST("/QueryContract_incompany", v1.QueryContract_incompany)
-		apiV1.POST("/ContractSanction_upload", v1.ContractSanction_upload)
-		apiV1.POST("/QueryContract_amongcompany", v1.QueryContract_amongcompany_1)
-		apiV1.POST("/QueryContract_state", v1.QueryContract_state)
+		apiV1.POST("/StartContract", v1.StartContract)//部门节点调用，创建合同
+		apiV1.POST("/QueryContract_incompany", v1.QueryContract_incompany)//部门节点和老板节点调用，查询公司内合同
+		apiV1.POST("/ContractSanction_upload", v1.ContractSanction_upload)//老板节点调用，签署合同并上传到公司间channel
+		apiV1.POST("/QueryContract_amongcompany", v1.QueryContract_amongcompany_1)//老板节点调用，查询公司间合同
+		apiV1.POST("/QueryContract_state", v1.QueryContract_state)//老板节点调用，查询某合同签署情况（在发起未签署、已经签署、为其他公司的合同、无此合同
 	}
 	return r
 }
